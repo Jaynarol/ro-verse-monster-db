5ff4d5d815de32b09ec6026b9cb8524ca9507d9a
Jaynarol <jaynarol@gmail.com>
2024-04-21 16:25:23 +0700

    fix deploy error can't load static file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+
+const REPO_PATH = 'ro-verse-monster-db'
+
 const nextConfig = {
   output: 'export',
   trailingSlash: false,
   skipTrailingSlashRedirect: true,
   images: { unoptimized: true }
 };
+
+
+if (process.env.NODE_ENV !== "development") {
+  const assetPrefix = `/${REPO_PATH}/`
+  const basePath = `/${REPO_PATH}`
+  nextConfig.assetPrefix = assetPrefix
+  nextConfig.basePath = basePath
+}
 
 export default nextConfig;
